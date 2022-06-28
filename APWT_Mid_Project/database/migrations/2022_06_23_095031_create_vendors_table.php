91925ba83cb314622374a78d3104c4b921d106d9
Md Al Amin Hossen <aamin.hossen99@gmail.com>
2022-06-29 01:38:10 +0600

    v removed

--- APWT_Mid_Project/database/migrations/2022_06_23_095031_create_vendors_table.php
+++ APWT_Mid_Project/database/migrations/2022_06_23_095031_create_vendors_table.php
@@ -18,12 +18,12 @@ class CreateVendorsTable extends Migration
             $table->string('v_username', 100)->unique();
             $table->string('v_name', 100);
             $table->string('v_email', 100)->unique();
-            $table->integer('v_phone');
-            $table->string('v_password', 100);
-            $table->string('v_gender');
-            $table->string('v_dob');
-            $table->string('v_address', 300);
-            $table->string('v_propic')->nullable(); //For Nullable Value -MR
+            $table->integer('phone');
+            $table->string('password', 100);
+            $table->string('gender');
+            $table->string('dob');
+            $table->string('address', 300);
+            $table->string('propic')->nullable(); //For Nullable Value -MR
             $table->timestamp('created_at')->nullable();
             $table->timestamp('updated_at')->nullable();
         });
