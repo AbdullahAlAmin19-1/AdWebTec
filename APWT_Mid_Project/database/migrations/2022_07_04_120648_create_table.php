7808368d686fd3201e073f8469b32a5765fdd349
AbdullahAlAmin19-1 <abdullahalamin1211@gmail.com>
2022-07-18 03:06:52 +0600

    create table

--- APWT_Mid_Project/database/migrations/2022_07_04_120648_create_table.php
+++ APWT_Mid_Project/database/migrations/2022_07_04_120648_create_table.php
@@ -220,6 +220,15 @@ class CreateTable extends Migration
         //     $table->timestamp('created_at')->nullable();
         //     $table->timestamp('updated_at')->nullable();
         // });
+        // Schema::create('req_coupons', function (Blueprint $table) {
+        //     $table->increments('id');
+        //     $table->string('code')->unique(); //integer to string -MR
+        //     $table->integer('amount');
+        //     $table->integer('v_id')->unsigned()->nullable(); //For Nullable Value -MR
+        //     $table->foreign('v_id')->references('id')->on('vendors');
+        //     $table->timestamp('created_at')->nullable();
+        //     $table->timestamp('updated_at')->nullable();
+        // });
 
         Schema::table('customers', function (Blueprint $table) {
             // $table->foreign('cco_id')->references('id')->on('customer_coupons');
