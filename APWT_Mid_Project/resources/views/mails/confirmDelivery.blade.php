10175f247443b280d86d8009c1174356f9b7b301
AbdullahAlAmin19-1 <abdullahalamin1211@gmail.com>
2022-07-18 01:20:53 +0600

    multiple review update

--- APWT_Mid_Project/resources/views/mails/confirmDelivery.blade.php
+++ APWT_Mid_Project/resources/views/mails/confirmDelivery.blade.php
@@ -13,15 +13,23 @@ Delivery address: {{$c_address}}<br>
 <table border="2px" style="width: 90%; border-collapse: collapse;">
     <tr>
         <th>Customer Name</th>
-        <th>Product Name</th>
-        <th>Product Quantity</th>
         <th>Delivery address</th>
     </tr>
     <tr>
         <th>{{$c_name}}</th>
-        <th>{{$p_name}}</th>
-        <th>{{$p_quantity}}</th>
         <th>{{$c_address}}</th>
     </tr>
 </table>
+<br> <br>
+
+        <table border="2px" style="width: 90%; border-collapse: collapse;">
+            <tr>
+                <th colspan="8">-- Delivery Details --</th>
+            </tr>
+            <tr>
+                <td colspan="8" style="text-align: center;"><b>Delivery Date:</b> Within 3-5 Days | <b>DeliveryMan:</b> <a href="#">{{$d_id}}</a></td>
+            </tr>
+        </table>
+
+        <h4>Thank You! <br>- Grocery OS</h4>
 </center>
