cc97b6bd7ee74b20732620ee90afbb4a38a23804
AbdullahAlAmin19-1 <abdullahalamin1211@gmail.com>
2022-07-30 02:32:17 +0600

    React

--- APWT_Final_Project/src/App.js
+++ APWT_Final_Project/src/App.js
@@ -1,19 +1,25 @@
-// routes
-import Router from './routes';
-// theme
-import ThemeProvider from './theme';
-// components
-import ScrollToTop from './components/ScrollToTop';
-import { BaseOptionChartStyle } from './components/chart/BaseOptionChart';
+import logo from './logo.svg';
+import './App.css';
 
-// ----------------------------------------------------------------------
-
-export default function App() {
+function App() {
   return (
-    <ThemeProvider>
-      <ScrollToTop />
-      <BaseOptionChartStyle />
-      <Router />
-    </ThemeProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
+export default App;
